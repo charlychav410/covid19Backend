dce60d5b1b95116694a87ed36a0a45d745f9c221
Gallardo <abelandresgallardo@hotmail.com>
2020-04-17 09:43:04 -0500

    Implementation of userRegisterService method was performed

--- src/main/java/com/softtek/academy/projectCOVID19/mainController/ControllerCovid.java
+++ src/main/java/com/softtek/academy/projectCOVID19/mainController/ControllerCovid.java
@@ -19,16 +19,17 @@ import com.google.gson.JsonObject;
 import com.softtek.academy.projectCOVID19.common.Validations;
 import com.softtek.academy.projectCOVID19.dataTransferObjects.AnwersSelectDTO;
 import com.softtek.academy.projectCOVID19.dataTransferObjects.LoginRequestDTO;
+import com.softtek.academy.projectCOVID19.dataTransferObjects.UserRegisterDTO;
 import com.softtek.academy.projectCOVID19.serviceMethods.InsertAnswersService;
 import com.softtek.academy.projectCOVID19.serviceMethods.UpdateAnswersService;
 import com.softtek.academy.projectCOVID19.serviceMethods.UserLoginService;
 import com.softtek.academy.projectCOVID19.serviceMethods.UserRegisterService;
 
 @RestController
-@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST })
 @EnableAutoConfiguration
 public class ControllerCovid {
-	
+
 	@Autowired
 	private UserLoginService userLoginService;
 	@Autowired
@@ -41,58 +42,47 @@ public class ControllerCovid {
 	private SelectAnswersService selectAnswersService;
 	@Autowired
 	private Validations validations;
-	
-	
-	@RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
+
+	@RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
 	String index() {
 		return "Bienvenido equipo Backend proyecto COVID19";
 	}
-	
-	
-	@PostMapping(value = {"/login"}, 
-			consumes = MediaType.APPLICATION_JSON_VALUE,
-			produces = MediaType.APPLICATION_JSON_VALUE)
-	Object login(@RequestBody LoginRequestDTO loginRequestDTO ) {
+
+	@PostMapping(value = {
+			"/login" }, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	Object login(@RequestBody LoginRequestDTO loginRequestDTO) {
 		return userLoginService.userLogin(loginRequestDTO.getIs(), loginRequestDTO.getPassword());
 	}
-	
-	
-	@PostMapping(value = {"/selectAnswers"},
-			     consumes = MediaType.APPLICATION_JSON_VALUE,
-			     produces = MediaType.APPLICATION_JSON_VALUE)
+
+	@PostMapping(value = {
+			"/selectAnswers" }, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	@Transactional(readOnly = true)
 	public ResponseEntity<?> selectAnswers(@RequestBody String is) {
 		AnwersSelectDTO dto = new AnwersSelectDTO();
 		JsonObject jObj = new Gson().fromJson(is, JsonObject.class);
-		
-		if(validations.validateIS(jObj.get("is").getAsString())) {
+
+		if (validations.validateIS(jObj.get("is").getAsString())) {
 			dto = selectAnswersService.selectA(jObj.get("is").getAsString());
-		}else {
+		} else {
 			dto.setMessage("IS incorrecto");
-			return new ResponseEntity<>(dto,HttpStatus.CONFLICT);
+			return new ResponseEntity<>(dto, HttpStatus.CONFLICT);
 		}
-		
-		return new ResponseEntity<>(dto,HttpStatus.OK);
+
+		return new ResponseEntity<>(dto, HttpStatus.OK);
 	}
-	
-	
-	@RequestMapping(value = {"/userRegister"}, method = RequestMethod.POST)
-	String userRegister() {
-		return "Servicio para registrar usuarios "+userRegisterService.validateAllParameters("params");
+
+	@RequestMapping(value = { "/userRegister" }, method = RequestMethod.POST)
+	String userRegister(@RequestBody UserRegisterDTO userRegisterDTO) {
+		return userRegisterService.userInsert(userRegisterDTO);
 	}
-	
-	
-	@RequestMapping(value = {"/insertAnswers"}, method = RequestMethod.POST)
+
+	@RequestMapping(value = { "/insertAnswers" }, method = RequestMethod.POST)
 	String insertAnswers() {
 		return "Servicio para insertar respuestas";
 	}
-	
-	
-	@RequestMapping(value = {"/updateAnswers"}, method = RequestMethod.POST)
+
+	@RequestMapping(value = { "/updateAnswers" }, method = RequestMethod.POST)
 	String updateAnswers() {
 		return "Servicio para actualizar respuestas";
 	}
-	
-	
-
 }
