e3d05e370d78f46534ff03a3383ec3af373ac048
Gallardo <abelandresgallardo@hotmail.com>
2020-04-17 09:31:34 -0500

    Added methods for insertion to DB In the interface the method was added that makes direct insertion already with the encryption of the password and in the class it was implemented with the validations

--- src/main/java/com/softtek/academy/projectCOVID19/repositoryMethods/PeopleRE.java
+++ src/main/java/com/softtek/academy/projectCOVID19/repositoryMethods/PeopleRE.java
@@ -1,6 +1,8 @@
 package com.softtek.academy.projectCOVID19.repositoryMethods;
 
+import javax.transaction.Transactional;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -13,4 +15,9 @@ public interface PeopleRE extends CrudRepository<People,String>{
 	
 	 @Query(value="select AES_DECRYPT(p.PASSWORD_P,'KEY') from PEOPLE p where p.IS = :is", nativeQuery = true)
 	  String findByIs(String is);
+	
+	 @Modifying
+     @Transactional 
+     @Query(value ="insert into PEOPLE (`IS`,`NAME`,`LAST_NAME`,`LAST_NAME_M`,`EMAIL`,`PHONE`,`PASSWORD_P`) values (:is,:name,:lastName,:lastNameM,:email,:phone,aes_encrypt(:pass, 'KEY'))",nativeQuery = true)
+    void insertUserQ(String is,String name,String lastName,String lastNameM,String email,String phone,String pass);
 }
