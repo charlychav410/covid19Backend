2c93a87e49de999d5031a338c6afd216c3f5e631
David <davisexa@gmail.com>
2020-04-22 17:06:41 -0500

    Modificacion del metodo de insertar "asnwers" Se modifico la recepcion de datos por parte del controlador ya que solo se hacia una insercion por cada peticion

--- src/main/java/com/softtek/academy/projectCOVID19/serviceMethods/InsertAnswersService.java
+++ src/main/java/com/softtek/academy/projectCOVID19/serviceMethods/InsertAnswersService.java
@@ -1,9 +1,9 @@
 package com.softtek.academy.projectCOVID19.serviceMethods;
 
-import com.softtek.academy.projectCOVID19.dataTransferObjects.AnswersInsertDTO;
+import com.softtek.academy.projectCOVID19.dataTransferObjects.AnswersSaveDTO;
 
 public interface InsertAnswersService {
 	boolean validateParameters(String params);
 	boolean validateAnsw(String params);
-	String insertAns(AnswersInsertDTO ans);
+	Object insertAns(AnswersSaveDTO ans);
 }
